b2f52be863d487034a1a94b91784b5f1b11721f8
nothingdev <nothingtillsomething@gmail.com>
2023-06-10 19:22:07 +0100

    First Commit

---
+++ src/app/Youtube.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react';
+import YouTube, { YouTubeProps } from 'react-youtube';
+
+export default function Youtube() {
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '400',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      loop: 1
+    },
+  };
+
+  return <YouTube videoId="j40YC5d6RzA" opts={opts} onReady={onPlayerReady} />;
+}
