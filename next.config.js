170b5cb86e4cdbba512187b6015f6bc1e7a8f947
nothingdev <nothingtillsomething@gmail.com>
2023-06-10 19:44:19 +0100

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
+const config = require("./src/config/config.json");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  basePath: config.base_path !== "/" ? config.base_path : "",
+  trailingSlash: config.site.trailing_slash,
   eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    }, 
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    }
-  
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+
 }
 
-module.exports = nextConfig
+
+module.exports = nextConfig;
